b1f8d028e14bdf247efe417a0dc67dd24d5273b9
oganov2001 <georgeoganov@hotmail.com>
2022-03-09 15:18:27 +0200

    Added Classes

---
+++ classes.js
@@ -0,0 +1,26 @@
+class Car {
+  constructor({ title }) {
+     this.title = title;
+  }
+
+  drive() {
+    return 'vroom';
+  }
+}
+
+
+class Toyota extends Car {
+  constructor(options) {
+    super(options);
+    this.color = options.color;
+  }
+
+  honk() {
+    return 'beep';
+  }
+}
+
+const toyota = new Toyota({ color: 'red', title: 'Daily Driver'});
+console.log(toyota.honk());
+console.log(toyota.drive());
+console.log(toyota);
